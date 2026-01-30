2104569e55ddfe981cb6d01d659b13464c80ec8e
acliang1412 <acliang7229@gmail.com>
2026-01-29 19:37:06 -0600

    Add jQuery for enhanced navbar functionality and implement scroll-based header shrink effect

--- ACL_Repository/JAVASCRIPT-PROJECTS/Academy-Cinema/JavaScript/javascript.js
+++ ACL_Repository/JAVASCRIPT-PROJECTS/Academy-Cinema/JavaScript/javascript.js
@@ -31,3 +31,30 @@ function displaySelectedMovieInModal() {
 function buyTickets(){
     displaySelectedMovieInModal();
 }
+
+//JQUERY
+
+//Shrink header size when the document is scrolled down by 80px
+$(document).on("scroll", function() {
+    //When the webpage is scrolled down from the top by 50px 
+    //if statement will trigger
+    if ($(document).scrollTop()>50) {
+        //Once the 50px requirement is met, add nav-shrink class selector to the same HTML element that has the nav class
+        $("nav").addClass("nav-shrink");
+        //Adjust the position of the movile drop meny to accommodate the new height decrease
+        $("div.navbar-collapse").css("margin-top", "-6px");
+    } else {
+        //If the webpage has not been scrolled down or is back at the top the nav-shrink class selector is removed from the HTML element with the nav class selector
+        $("nav").removeClass("nav-shrink");
+        //Reset the position of the mobile drop menu to its original position
+        $("div.navbar-collapse").css("margin-top", "14px");
+    }
+});
+
+//Close mobile meny when a nav link is clicked 
+$(document).ready(function(){
+    //On click when and element contains just the nav-link class and not the dropdown0toggle and then also close when an element with the class .dropdown-item is clicked (each movie link) has been clicked
+    $(".navbar-nav").on("click", ".nav-link:not(.dropdown-toggle), .dropdown-item", function(){
+        $(".navbar-collapse").collapse("hide");
+    });
+});
