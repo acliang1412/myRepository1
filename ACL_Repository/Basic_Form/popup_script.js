c3daeccd81808c692863f160b2922cc91383cec8
acliang1412 <acliang7229@gmail.com>
2026-02-03 19:32:25 -0600

    Add popup contact form with styling and functionality

---
+++ ACL_Repository/Basic_Form/popup_script.js
@@ -0,0 +1,7 @@
+function openForm() {
+  document.getElementById("myFormPopup").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myFormPopup").style.display = "none";
+}
